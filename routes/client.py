5bbb530ca8e4e84a6135d41e2f9a5f0e66738e90
Lucas <alcantaralucas2002@gmail.com>
2025-07-27 18:07:16 -0300

    Add: License, requirements.tx, Item_clients.html, and modified other lines and files about routes.

--- routes/client.py
+++ routes/client.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from database.cliente import CLIENTS
 
 
@@ -12,30 +12,42 @@ def clients_list():
 @client_route.route('/', methods=['POST'])
 def insert_client():
     """ Insert the data of a new client into the database """
-    pass
-
+    
+    data = request.json
+    
+    new_user = {
+        "id": len(CLIENTS) + 1,
+        "name": data['name'],
+        "email": data['email'],
+    }
+    
+    CLIENTS.append(new_user)
+    
+    return render_template('item_client.html', client=new_user)
+    
 @client_route.route('/new')
 def form_client():
     """ Form to register a new client """
     return render_template('form_client.html')
 
 @client_route.route('/<int:client_id>')
-def client_details(id):
+def client_details(client_id):
     """ Show the details of a client """
     return render_template('client_details.html')
 
 @client_route.route('/<int:client_id>/edit')
-def form_edit_client(id):
+def form_edit_client(client_id):
     """ Form to edit the data of a client """
     return render_template('form_edit_client.html')
 
 @client_route.route('/<int:client_id>/update', methods=['PUT'])
-def update_client(id):
+def update_client(client_id):
     """ Update the data of a client in the database """
     pass
 
 
 @client_route.route('/<int:client_id>/delete', methods=['DELETE'])
-def delete_client(id):
+def delete_client(client_id):
     """ Delete a client from the database """
-    pass
+
+    CLIENTS = [ c for c in CLIENTS if c['id'] != client_id ]
